6b0c0d9e647e1098f1b489fd06a0f5a9e9b4a6c0
Sajid Ashraf <sajidashraf.dev@gmail.com>
2025-09-30 22:07:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+const BuyNowBtns = document.getElementsByClassName('hero-btn');
+for (let i = 0; i < BuyNowBtns.length; i++) {
+    BuyNowBtns[i].addEventListener('click', function() {
+        window.location.href = 'form-page.html';
+    });
+}
